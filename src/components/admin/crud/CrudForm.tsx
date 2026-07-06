18298b637a8a53c21d18ae10067040aee3562add
YASH PAL <2004yashpal@gmail.com>
2026-07-06 16:11:43 +0530

    fix: import Button in CrudForm.tsx

--- src/components/admin/crud/CrudForm.tsx
+++ src/components/admin/crud/CrudForm.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm, Controller, useFormContext } from "react-hook-form";
+import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
